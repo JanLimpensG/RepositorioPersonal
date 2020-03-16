ce1e681d1284d5e28bde001ac6bbe05bc07911d2
JanLimpensG <jan_limpens@hotmail.com>
2020-03-16 16:30:10 -0600

    Lab 10 completo

--- Lab_10/confirmacion.php
+++ Lab_10/confirmacion.php
@@ -4,27 +4,35 @@ include("partials/_header.html");
 
 include("partials/_nav.html");
 
+$nom = htmlspecialchars($_POST["nombre"]);
+$ape = htmlspecialchars($_POST["apellido"]);
+$tel = htmlspecialchars($_POST["telefono"]);
+$con = htmlspecialchars($_POST["contra"]);
+$conf = htmlspecialchars($_POST["confirmContra"]);
+$corr = htmlspecialchars($_POST["mail"]);
+$fena = htmlspecialchars($_POST["fecha_nac"]);
+
 echo '<div class="container">';
-echo "<h3>Gracias </h3><p>".$_POST["nombre"]." ".$_POST["apellido"]." por confiar en nuestra pagina, ";
+echo "<h3>Gracias </h3><p>".$nom." ".$ape." por confiar en nuestra pagina, ";
 echo "a continuacion se monstrara tu información para que puedas confirmarla</p>";
 
 echo "<h3> Confirme sus datos</h3>";
 echo "<h4> Contraseña </h4>";
 
-if($_POST["contra"] <> $_POST["confirmContra"]){
+if($con <> $conf){
 	echo "<p>LA CONTRASEÑA QUE PUSISTE NO CONCUERDA CON LA CONTRASEÑA DE CONFIRMACION</p>";
 } else {
 	echo "<p> La contraseña seleccionada concuerda con la conifirmacion </p>";
 }
 
 echo "<h4> Correo electrónico </h4>";
-echo "<p>Tu correo electrónico es: ".$_POST["mail"]."</p>";
+echo "<p>Tu correo electrónico es: ".$corr."</p>";
 
 echo "<h4>Teléfono </h4>";
-echo "<p> El teléfono que pusiste es: ".$_POST["telefono"]."</p>";
+echo "<p> El teléfono que pusiste es: ".$tel."</p>";
 
 echo "<h4> Fecha de nacimiento</h4>";
-echo "<p> Tu fecha de nacimiento es: ".$_POST["fecha_nac"]."</p>";
+echo "<p> Tu fecha de nacimiento es: ".$fena."</p>";
 
 include("partials/_confirma.html");
 
