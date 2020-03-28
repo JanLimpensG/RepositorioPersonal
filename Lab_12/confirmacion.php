17431f8903a4d8c4b18a2629c0f38adbfcb2fbef
JanLimpensG <jan_limpens@hotmail.com>
2020-03-28 12:58:36 -0600

    Lab 12 falta mostrar imagen subida

--- Lab_12/confirmacion.php
+++ Lab_12/confirmacion.php
@@ -2,21 +2,6 @@
 
 session_start();
 
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
 
 include("partials/_header.html");
 
@@ -28,7 +13,7 @@ $_SESSION["con"] = htmlspecialchars($_POST["contra"]);
 
 echo '<div class="container">';
 echo "<h3> Informacion de la sesión:  </h3><p> Acutalmente la sesion pertenece a: ".$_SESSION["nom"]." con nombre de usuario:  ".$_SESSION["usu"].", gracias por confiar en nuestra pagina, ";
-echo "a continuacion se monstrara tu información para que puedas confirmarla</p>";
+echo "a continuacion se mostrara tu información para que puedas confirmarla</p>";
 
 
 echo "<h4> Constraseña </h4>";
@@ -39,7 +24,7 @@ echo "<p>Tu foto es : ".$_SESSION["con"] ."</p>";
 
 
 
-include("partials/_confirma.html");
+include("partials/_formFoto.html");
 
 
 echo '</div>';
